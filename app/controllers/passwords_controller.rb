28ee5be813e3d5a22911c4d4ad685315eb4074a9
Wojtek Mikusek <miqs1992@gmail.com>
2019-11-28 08:55:23 +0100

    [CAZ-1253] Add placeholder for password reset

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class PasswordsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def new; end
 
   def create
     # Storing the password?
-    redirect_to add_users_path
+    redirect_to root_path
   end
 end
