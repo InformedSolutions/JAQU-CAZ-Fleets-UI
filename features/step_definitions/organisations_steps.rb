b282f94a1bf6f6169df5134ffbe291099e4da326
Sergii Chub <s.chub@exlabs.pl>
2019-12-10 14:41:30 +0200

    [CAZ-1564] Add connect to account create endpoint (#25)

--- features/step_definitions/organisations_steps.rb
+++ features/step_definitions/organisations_steps.rb
@@ -20,10 +20,19 @@ Then('I enter a company name') do
 end
 
 And('I enter the account details') do
-  fill_in('organisations_email', with: 'example@email.com')
-  fill_in('organisations_email_confirmation', with: 'example@email.com')
-  fill_in('organisations_password', with: 'CboD9%1Q')
-  fill_in('organisations_password_confirmation', with: 'CboD9%1Q')
+  allow(AccountsApi).to receive(:create_account).and_return(read_response('create_account.json'))
+  fill_account_details
+end
+
+And('I enter the account details with not uniq email address') do
+  stub_request(:post, /accounts/).to_return(
+    status: 422,
+    body: {
+      'message': 'Submitted parameters are invalid',
+      'details': %w[emailNotUnique passwordNotValid emailNotValid]
+    }.to_json
+  )
+  fill_account_details
 end
 
 When('I go to the email verified page') do
@@ -36,4 +45,11 @@ end
 
 Then('I should receive verification email again') do
   expect(Sqs::VerificationEmail).to have_received(:call).twice
+end
+
+def fill_account_details
+  fill_in('organisations_email', with: 'example@email.com')
+  fill_in('organisations_email_confirmation', with: 'example@email.com')
+  fill_in('organisations_password', with: 'CboD9%1Q')
+  fill_in('organisations_password_confirmation', with: 'CboD9%1Q')
 end
--- spec/services/create_account_spec.rb
+++ spec/services/create_account_spec.rb
@@ -15,38 +15,61 @@ describe CreateAccount do
       password_confirmation: password
     )
   end
-  let(:user) { create_user(email: email, account_name: company_name) }
+  let(:user) { create_admin(email: email, account_name: company_name) }
   let(:email) { 'email@example.com' }
   let(:password) { '8NAOTpMkx2%9' }
   let(:company_name) { 'Mikusek Software' }
   let(:host) { 'www.example.com' }
   let(:valid) { true }
 
-  before do
-    allow(EmailAndPasswordForm)
-      .to receive(:new)
-      .and_return(instance_double(EmailAndPasswordForm, valid?: valid))
-    allow(AccountsApi).to receive(:create_organization).and_return(user)
-    allow(Sqs::VerificationEmail).to receive(:call).and_return(SecureRandom.uuid)
+  context 'when api returns correct response' do
+    before do
+      allow(EmailAndPasswordForm)
+        .to receive(:new)
+        .and_return(instance_double(EmailAndPasswordForm, valid?: valid))
+      response = read_response('create_account.json')
+      allow(AccountsApi).to receive(:create_account).and_return(response)
+      allow(Sqs::VerificationEmail).to receive(:call).and_return(SecureRandom.uuid)
+    end
+
+    it 'returns the User class' do
+      expect(service.class).to eq(User)
+    end
+
+    it 'calls EmailAndPasswordForm with proper params' do
+      expect(EmailAndPasswordForm).to receive(:new).with(params)
+      service
+    end
+
+    it 'calls AccountsApi.create_account with proper params' do
+      expect(AccountsApi)
+        .to receive(:create_account)
+        .with(
+          email: email,
+          password: password,
+          company_name: company_name
+        )
+      service
+    end
   end
 
-  it 'returns the user' do
-    expect(service).to eq(user)
-  end
+  context 'when api returns 442 status' do
+    before do
+      allow(EmailAndPasswordForm)
+        .to receive(:new)
+        .and_return(instance_double(EmailAndPasswordForm, valid?: valid))
 
-  it 'calls EmailAndPasswordForm with proper params' do
-    expect(EmailAndPasswordForm).to receive(:new).with(params)
-    service
-  end
-
-  it 'calls AccountsApi.create_organization with proper params' do
-    expect(AccountsApi)
-      .to receive(:create_organization)
-      .with(
-        email: email,
-        _password: password,
-        _company_name: company_name
+      stub_request(:post, /accounts/).to_return(
+        status: 422,
+        body: {
+          "message": 'Submitted parameters are invalid',
+          "details": %w[emailNotUnique passwordNotValid emailNotValid parameterMissing]
+        }.to_json
       )
-    service
+    end
+
+    it 'raises `NewPasswordException` exception' do
+      expect { service }.to raise_error(NewPasswordException)
+    end
   end
 end
