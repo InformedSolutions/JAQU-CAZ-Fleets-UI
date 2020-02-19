951128dd45c3ba652c2b693d502d20442681f785
Sergii Chub <s.chub@exlabs.pl>
2020-02-19 15:03:05 +0100

    [CAZB-560] Update cookies control csp (#95)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,10 +6,7 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-#:nocov:
 if Rails.env.production?
-  cookie_control_url = %w[https://cc.cdn.civiccomputing.com]
-  ga_urls = %w[https://www.googletagmanager.com https://www.google-analytics.com]
   defaults = %i[self https]
   defaults.push(ENV['CLOUDFRONT_ENDPOINT']) if ENV['CLOUDFRONT_ENDPOINT']
 
@@ -18,7 +15,7 @@ if Rails.env.production?
     policy.font_src(*defaults, :data)
     policy.img_src(*defaults)
     policy.object_src(:none)
-    policy.script_src(*defaults, *ga_urls, *cookie_control_url, :unsafe_inline)
+    policy.script_src(*defaults, :unsafe_inline)
     policy.style_src(*defaults, :unsafe_inline)
     policy.connect_src(*defaults)
     policy.frame_ancestors(:none)
