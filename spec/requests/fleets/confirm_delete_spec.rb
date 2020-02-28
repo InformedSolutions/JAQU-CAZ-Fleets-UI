08443d0b99c88b3e59a1b2102f5965a29a93ee18
Wojtek Mikusek <miqs1992@gmail.com>
2020-02-28 10:48:59 +0100

    [CAZ-2007] Return to dashboard after last vehicle removal

--- spec/requests/fleets/confirm_delete_spec.rb
+++ spec/requests/fleets/confirm_delete_spec.rb
@@ -41,6 +41,14 @@ describe 'FleetsController - #confirm_delete', type: :request do
           http_request
           expect(session[:vrn]).to eq(nil)
         end
+
+        context 'when it was the last vehicle' do
+          it 'redirects to fleets page' do
+            mock_fleet(create_empty_fleet)
+            http_request
+            expect(response).to redirect_to(dashboard_path)
+          end
+        end
       end
 
       context 'when user does not confirm details' do
