c32800a68fa45785a5d9a61ffede96de8c29f0de
Antoni <a.pstras@exlabs.pl>
2020-03-13 12:42:49 +0100

    [CAZ-2043] Move spec to proper directory (#150)

--- spec/services/session_manipulation/set_payment_details_spec.rb
+++ spec/services/session_manipulation/set_payment_details_spec.rb
@@ -12,23 +12,23 @@ RSpec.describe SessionManipulation::SetPaymentDetails do
     )
   end
 
-  let(:session) { { new_payment: {} } }
+  let(:session) { { initiated_payment: {} } }
   let(:email) { 'test@example.com' }
   let(:payment_reference) { 1 }
   let(:external_id) { 'external payment id' }
 
   it 'sets email' do
     service
-    expect(session[:new_payment]['user_email']).to eq(email)
+    expect(session[:initiated_payment][:user_email]).to eq(email)
   end
 
   it 'sets payment reference' do
     service
-    expect(session[:new_payment]['payment_reference']).to eq(payment_reference)
+    expect(session[:initiated_payment][:payment_reference]).to eq(payment_reference)
   end
 
   it 'sets email' do
     service
-    expect(session[:new_payment]['external_id']).to eq(external_id)
+    expect(session[:initiated_payment][:external_id]).to eq(external_id)
   end
 end
