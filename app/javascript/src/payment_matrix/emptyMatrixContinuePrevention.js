a04a98aa055dfbed1ff322ff78b4f343399c9c26
Antoni Pstraś <antek.pstras@gmail.com>
2020-03-14 17:35:10 +0100

    [NT] Prevent from submitting payment with emtpy dates

---
+++ app/javascript/src/payment_matrix/emptyMatrixContinuePrevention.js
@@ -0,0 +1,28 @@
+export default function () {
+  recalculateContinueButtonDisability();
+  attachRecalculationToCheckboxes();
+}
+
+function attachRecalculationToCheckboxes() {
+  var allCheckboxes = document.getElementsByClassName('govuk-checkboxes__input');
+
+  for(var checkbox of allCheckboxes) {
+    checkbox.addEventListener('click', recalculateContinueButtonDisability);
+  }
+}
+
+function recalculateContinueButtonDisability() {
+  var allCheckboxes = document.getElementsByClassName('govuk-checkboxes__input');
+  var selectedCheckboxesCount = countCheckedCheckboxes(allCheckboxes);
+  var continueButton = document.getElementById('continue-matrix-button');
+
+  continueButton.disabled = selectedCheckboxesCount === 0;
+}
+
+function countCheckedCheckboxes(checkboxes) {
+  var checkedCheckboxesCount = 0;
+  for(var checkbox of checkboxes) {
+    if (checkbox.checked) checkedCheckboxesCount++;
+  }
+  return checkedCheckboxesCount;
+}
--- app/javascript/src/payment_matrix/initPaginationButton.js
+++ app/javascript/src/payment_matrix/initPaginationButton.js
@@ -5,7 +5,6 @@ export default function (direction) {
     const paymentForm = document.getElementById('payment-form');
     const paginationButton = document.getElementById(`pagination-${direction}-link`);
 
-
     if(paginationButton){
         paginationButton.addEventListener("click", (event) => {
             event.preventDefault();
