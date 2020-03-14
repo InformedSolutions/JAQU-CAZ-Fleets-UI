bf73615355ded8d642b73958a9501709ac12e839
Antoni Pstraś <antek.pstras@gmail.com>
2020-03-14 15:12:08 +0100

    [CAZ-2153] Payment Complete - City Council links

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -51,4 +51,15 @@ module PaymentsHelper
   def single_vrn_parsed_charge(dates, charge)
     "£#{dates.length * charge}"
   end
+
+  # Returns proper URL based on provided parameters
+  def charge_alternative_url_for(city_name, account_type = 'fleet')
+    url = external_urls[city_name.downcase][account_type]
+    external_link_to("#{city_name.titleize} City Council Website", url)
+  end
+
+  # Reads file with additional URLs
+  def external_urls
+    YAML.load_file('additional_urls.yaml')
+  end
 end
