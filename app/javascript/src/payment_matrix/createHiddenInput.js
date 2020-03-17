908cc8561dc1e932676f4e568d7215256cc1d389
James Cruddas <james.cruddas@informed.com>
2020-03-17 23:04:40 +0000

    Remove all whitespace from search input value
    This previously would raise error on a search being triggered. The regex
    approach supports vrns with whitespace inside (rather than a simply trim on the string)

--- app/javascript/src/payment_matrix/createHiddenInput.js
+++ app/javascript/src/payment_matrix/createHiddenInput.js
@@ -2,6 +2,6 @@ export default function(name, value) {
     const input = document.createElement("input");
     input.type = "hidden";
     input.name = name;
-    input.value = value.toUpperCase();
+    input.value = value.replace(/\s/g, "").toUpperCase();
     return input;
 };
