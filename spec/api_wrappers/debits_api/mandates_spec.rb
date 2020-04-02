9d9347388ae98a4c3ac7f94ca0dbf7f1cd5352a2
Sergii Chub <s.chub@exlabs.pl>
2020-04-02 11:34:25 +0200

    [CAZ-2299] Link capitalisation on Vehicle Management page (#185)

--- spec/api_wrappers/debits_api/mandates_spec.rb
+++ spec/api_wrappers/debits_api/mandates_spec.rb
@@ -2,18 +2,17 @@
 
 require 'rails_helper'
 
-describe 'DebitsApi.caz_mandates' do
-  subject(:call) { DebitsApi.caz_mandates(account_id: account_id, zone_id: zone_id) }
+describe 'DebitsApi.mandates' do
+  subject(:call) { DebitsApi.mandates(account_id: account_id) }
 
   let(:account_id) { SecureRandom.uuid }
-  let(:zone_id) { SecureRandom.uuid }
-  let(:base_url) { "payments/accounts/#{account_id}/direct-debit-mandates/#{zone_id}" }
+  let(:base_url) { "payments/accounts/#{account_id}/direct-debit-mandates" }
 
   context 'when the response status is 200' do
     before do
       stub_request(:get, /#{base_url}/).to_return(
         status: 200,
-        body: read_unparsed_response('debits/caz_mandates.json')
+        body: read_unparsed_response('debits/mandates.json')
       )
     end
 
