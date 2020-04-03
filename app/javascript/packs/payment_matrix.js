a990d173a3de9f3a8451a38e6403ccfe9e7cd1ed
Sergii Chub <s.chub@exlabs.pl>
2020-04-03 19:09:43 +0200

    [CAZ-2268] Add compatibility between IE and babel javascript (#188)

--- app/javascript/packs/payment_matrix.js
+++ app/javascript/packs/payment_matrix.js
@@ -1,3 +1,5 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import initSearch from "../src/payment_matrix/initSearch";
 import initPaginationButton from "../src/payment_matrix/initPaginationButton";
 import preventContinue from "../src/payment_matrix/emptyMatrixContinuePrevention";
