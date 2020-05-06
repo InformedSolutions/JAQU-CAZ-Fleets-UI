3f8b9365d4db03a1500f10b497f1ad7057684195
dratka <d.ratka@exlabs.pl>
2020-05-06 13:44:17 +0200

    [CAZ-967] Fix Fleet check when not checked

--- app/helpers/fleets_helper.rb
+++ app/helpers/fleets_helper.rb
@@ -4,4 +4,8 @@ module FleetsHelper
   def zone_colspan(index)
     index == (@zones.size - 1) ? 2 : 1
   end
+
+  def checked_fleet_checked?(value)
+    session.dig('new_account', 'confirm_fleet_check').to_s == value
+  end
 end
