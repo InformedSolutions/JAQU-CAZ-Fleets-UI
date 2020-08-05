6e9c341a45906f0cbcaa404a94c59424c1dc0a8b
Sergii Chub <s.chub@exlabs.pl>
2020-08-05 10:35:46 +0200

    [CAZ-2400] Show himself on the list but without Change link (#455)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,11 @@
 ##
 # Module used for manage users flow
 module UsersHelper
+  # Checks if current user `user_id` not equals to parameter `user_id`
+  def not_own_account?(user_id)
+    current_user.user_id != user_id
+  end
+
   # Checks if user dont want to edit or remove his own account
   # If yes redirects to the Manage users page
   def check_account_ownership?(user_id)
