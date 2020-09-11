d850c11d6ebcbc3cf780e7c9c6098a07991f6322
Piotr Kałamarski <pkalamarski@gmail.com>
2020-09-11 11:59:22 +0200

    Logout controller tests (#545)

---
+++ spec/requests/logout_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe LogoutController do
+  describe 'GET #sign_out' do
+    subject { get sign_out_path }
+
+    it 'renders correct template' do
+      expect(subject).to render_template('devise/sessions/sign_out')
+    end
+  end
+
+  describe 'POST #assign_logout_notice_back_url' do
+    subject { post assign_logout_notice_back_url_path(params: params) }
+
+    let(:params) { { logout_notice_back_url: root_path } }
+    before { subject }
+
+    it 'assigns correct value to session' do
+      expect(session[:logout_notice_back_url]).to eq(root_path)
+    end
+
+    it 'returns 204 response' do
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+
+  describe 'GET #logout_notice' do
+    subject { get logout_notice_path }
+
+    before { add_to_session(logout_notice_back_url: root_path) }
+
+    it 'renders correct template' do
+      expect(subject).to render_template('devise/sessions/logout_notice')
+    end
+
+    it 'assigns @back_button_url variable' do
+      subject
+      expect(assigns(:back_button_url)).to eq(root_path)
+    end
+  end
+
+  describe 'GET #timedout_user' do
+    subject { get timedout_user_path }
+
+    before do
+      sign_in create_user
+      subject
+    end
+
+    it 'redirects to login page' do
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'sets correct error to flash[:notice]' do
+      expect(flash[:notice]).to eq(I18n.t('inactivity_logout'))
+    end
+  end
+end
