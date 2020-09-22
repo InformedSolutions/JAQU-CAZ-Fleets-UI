b1a42e51e30fbac75530ebc1379569536cf3fc5b
Sergii Chub <s.chub@exlabs.pl>
2020-09-22 13:45:28 +0200

    Remove unused settings for simplecov and sonar

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 REDIS = if Rails.env.production? && ENV['REDIS_URL']
-          # :nocov:
           Redis.new(cluster: [ENV['REDIS_URL']])
-          # :nocov:
         else
           Redis.new
         end
