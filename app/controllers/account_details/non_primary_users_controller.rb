04cbe756eaf3d919cb31a3bb7751b44374ab7ec9
Antoni Pstraś <antek.pstras@gmail.com>
2020-09-23 14:21:34 +0200

    [CAZB-2633] Account details link on dashboard

---
+++ app/controllers/account_details/non_primary_users_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+##
+# Module used for account details management
+module AccountDetails
+  ##
+  # Controller used to manage the non-primary user settings
+  #
+  class NonPrimaryUsersController < ApplicationController
+    ##
+    # Renders the management page for non-primary users.
+    #
+    # ==== Path
+    #
+    #    :GET /non_primary_user
+    #
+    def index
+      # renders a static page
+    end
+  end
+end
