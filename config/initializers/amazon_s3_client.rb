e164901dacf23a5084cc565efd3d6286ceada68b
Krzysztof Szromek <1729299+szromek@users.noreply.github.com>
2020-09-28 13:56:14 +0200

    Revert "Enable S3 connection on local environment through STS Assume Role"
    This reverts commit 5d4eff5ea31bd22ee522157229802792875f196e.

--- config/initializers/amazon_s3_client.rb
+++ config/initializers/amazon_s3_client.rb
@@ -1,34 +1,15 @@
 # frozen_string_literal: true
 
-# Development environment config
-access_key_id = ENV['AWS_ACCESS_KEY_ID']
-secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-assume_role_arn = ENV['AWS_ROLE_ARN']
-assume_role_session_name = 'jaqu-lowerDeveloperRole-localdev'
-
-# Production environment config
-TASK_METADATA_ADDRESS = '169.254.170.2'
-
-# Common environment config
-region = ENV.fetch('AWS_REGION', 'eu-west-2')
-
-credentials = if Rails.env.production?
-                Aws::ECSCredentials.new({ ip_address: TASK_METADATA_ADDRESS })
-              elsif Rails.env.test?
-                Aws::Credentials.new(access_key_id, secret_access_key)
-              else
-                Aws::AssumeRoleCredentials.new(
-                  client: Aws::STS::Client.new(
-                    region: region,
-                    access_key_id: access_key_id,
-                    secret_access_key: secret_access_key
-                  ),
-                  role_arn: assume_role_arn,
-                  role_session_name: assume_role_session_name
+credentials = if ENV['S3_AWS_ACCESS_KEY_ID'] && ENV['S3_AWS_SECRET_ACCESS_KEY']
+                Aws::Credentials.new(
+                  ENV.fetch('S3_AWS_ACCESS_KEY_ID', 'S3_AWS_ACCESS_KEY_ID'),
+                  ENV.fetch('S3_AWS_SECRET_ACCESS_KEY', 'S3_AWS_SECRET_ACCESS_KEY')
                 )
+              else
+                Aws::ECSCredentials.new({ ip_address: '169.254.170.2' })
               end
 
 AMAZON_S3_CLIENT = Aws::S3::Resource.new(
-  region: region,
+  region: ENV.fetch('S3_AWS_REGION', 'eu-west-2'),
   credentials: credentials
 )
