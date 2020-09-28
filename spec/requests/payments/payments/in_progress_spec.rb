b10fb1a46619b0a809b0176abe69dcb1aae72151
Piotr Kałamarski <pkalamarski@gmail.com>
2020-09-28 13:33:24 +0200

    [CAZB-3185] Payment Lock back link fix (#591)

--- spec/requests/payments/payments/in_progress_spec.rb
+++ spec/requests/payments/payments/in_progress_spec.rb
@@ -6,32 +6,53 @@ describe 'PaymentsController - GET #in_progress' do
   subject { get in_progress_payments_path }
 
   let(:caz_id) { @uuid }
-  let(:user) { make_payments_user }
+  let(:account_id) { SecureRandom.uuid }
+  let(:user) { create_user(account_id: account_id, user_id: SecureRandom.uuid) }
+  let(:another_user) { create_user(account_id: account_id, user_id: SecureRandom.uuid) }
 
   context 'correct permissions' do
     let(:fleet) { create_chargeable_vehicles }
 
     before do
-      add_caz_lock_to_redis(user)
       sign_in user
+      mock_clean_air_zones
+      mock_fleet(fleet)
+      add_to_session(new_payment: { caz_id: caz_id })
     end
 
-    context 'with la in the session' do
+    context 'with la in the session and with no CAZ lock' do
       before do
-        mock_clean_air_zones
-        mock_fleet(fleet)
-        add_to_session(new_payment: { caz_id: caz_id })
+        add_caz_lock_to_redis(user)
+        subject
+      end
+
+      it 'returns a 302 FOUND status' do
+        expect(response).to have_http_status(:found)
+      end
+
+      it 'redirects to selected CAZ payment matrix' do
+        expect(subject).to redirect_to(matrix_payments_path)
+      end
+    end
+
+    context 'with la in the session and CAZ locked by other user' do
+      before do
+        add_caz_lock_to_redis(another_user)
+        subject
       end
 
       it 'returns a 200 OK status' do
-        subject
         expect(response).to have_http_status(:ok)
       end
     end
 
     context 'without la in the session' do
-      it 'redirects to the index' do
+      before do
+        add_to_session(new_payment: { caz_id: nil })
         subject
+      end
+
+      it 'redirects to the index' do
         expect(response).to redirect_to(payments_path)
       end
     end
