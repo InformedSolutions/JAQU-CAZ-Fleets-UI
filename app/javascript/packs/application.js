608a0a3c2541ab199960f2d1a3e8b917a7e9c13d
Antoni <a.pstras@exlabs.pl>
2020-09-28 11:57:23 +0200

    [CAZB-2819] Update name page (#588)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,9 +17,11 @@ import '../src/GovUKAssets';
 import { initAll } from 'govuk-frontend/govuk/all.js';
 import initPrintLink from "../src/printLink/init";
 import cookieControl from "../src/cookieControl";
+import initBackLink from '../src/backLink/init';
 
 document.body.classList.add('js-enabled');
 
 initAll();
 initPrintLink();
 cookieControl();
+initBackLink();
