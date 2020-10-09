53cb01a7621dfee48977dad22aadf7d4810b292d
Sergii Chub <s.chub@exlabs.pl>
2020-10-09 14:02:23 +0200

    [CAZB-3184] Check upload job status on the CAZ selection page (#637)

--- spec/requests/payments/payments/index_spec.rb
+++ spec/requests/payments/payments/index_spec.rb
@@ -5,17 +5,16 @@ require 'rails_helper'
 describe 'PaymentsController - GET #index' do
   subject { get payments_path }
 
-  before { sign_in create_user }
+  before { sign_in user }
+
+  let(:user) { make_payments_user }
 
   context 'correct permissions' do
     context 'with empty fleet' do
-      before do
-        mock_fleet(create_empty_fleet)
-      end
+      before { mock_fleet(create_empty_fleet) }
 
-      it 'redirects to first upload page' do
-        subject
-        expect(response).to redirect_to first_upload_fleets_path
+      it 'redirects to the first upload page' do
+        expect(subject).to redirect_to first_upload_fleets_path
       end
     end
 
@@ -25,9 +24,54 @@ describe 'PaymentsController - GET #index' do
         mock_fleet
       end
 
-      it 'renders payments page' do
-        subject
-        expect(response).to render_template(:index)
+      it 'renders the view' do
+        expect(subject).to render_template(:index)
+      end
+
+      context 'and with upload data in redis' do
+        before do
+          add_upload_job_to_redis
+          allow(FleetsApi).to receive(:job_status).and_return(status: status, errors: [])
+          subject
+        end
+
+        let(:upload_job_redis_key) { "account_id_#{user.account_id}" }
+
+        context 'and when status is SUCCESS' do
+          let(:status) { 'SUCCESS' }
+
+          it 'renders the view' do
+            expect(response).to render_template(:index)
+          end
+        end
+
+        context 'and when status is CHARGEABILITY_CALCULATION_IN_PROGRESS' do
+          let(:status) { 'CHARGEABILITY_CALCULATION_IN_PROGRESS' }
+
+          it 'redirects to the calculating chargeability page' do
+            expect(response).to redirect_to(calculating_chargeability_uploads_path)
+          end
+        end
+
+        context 'and when status is RUNNING' do
+          let(:status) { 'RUNNING' }
+
+          it 'redirects to the process uploading page' do
+            expect(response).to redirect_to(processing_uploads_path)
+          end
+        end
+
+        context 'and when status is unknown' do
+          let(:status) { 'UNKNOWN' }
+
+          it 'renders the view' do
+            expect(response).to render_template(:index)
+          end
+
+          it 'deletes job data from redis' do
+            expect(REDIS.hget(upload_job_redis_key, 'job_id')).to be_nil
+          end
+        end
       end
     end
   end
