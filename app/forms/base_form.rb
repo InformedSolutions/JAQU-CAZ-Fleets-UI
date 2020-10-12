9808a44d9cc4bdb35b260dfbecd824c033c4305d
Sergii Chub <s.chub@exlabs.pl>
2020-10-12 14:19:44 +0200

    [CAZB-3273] Allow all uppercase email letters (#645)

--- app/forms/base_form.rb
+++ app/forms/base_form.rb
@@ -8,7 +8,7 @@ class BaseForm
 
   # Email Address Regular Expression
   EMAIL_FORMAT = %r{\A(([\w\d!#$%&'*+\-/=?^_`{|}~]+)(\.{1}))*
-    ([\w\d!#$%&'*+\-/=?^_`{|}~]+)@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z}x.freeze
+    ([\w\d!#$%&'*+\-/=?^_`{|}~]+)@[a-zA-Z\d\-]+(\.[a-zA-Z\d\-]+)*\.[a-zA-Z]+\z}x.freeze
 
   # Overrides default initializer for compliance with form_for method in content_form view
   def initialize(attributes = {})
--- spec/forms/users_management/add_user_form_spec.rb
+++ spec/forms/users_management/add_user_form_spec.rb
@@ -13,7 +13,21 @@ describe UsersManagement::AddUserForm, type: :model do
   describe 'valid?' do
     before { allow(AccountsApi::Accounts).to receive(:user_validations).and_return(true) }
 
-    it { is_expected.to be_valid }
+    context 'valid email format' do
+      it { is_expected.to be_valid }
+
+      context 'when all uppercase letters' do
+        let(:email) { 'NEW_USER@EXAMPLE.COM' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'when uppercase letters only after @' do
+        let(:email) { 'new_user@EXAMPLE.COM' }
+
+        it { is_expected.to be_valid }
+      end
+    end
 
     context 'when name is empty' do
       let(:name) { '' }
@@ -26,53 +40,47 @@ describe UsersManagement::AddUserForm, type: :model do
       end
     end
 
-    context 'when email is empty' do
-      let(:email) { '' }
+    context 'invalid email format' do
+      before { subject.valid? }
 
-      it { is_expected.not_to be_valid }
+      context 'when email is empty' do
+        let(:email) { '' }
 
-      it 'has a proper error message' do
-        subject.valid?
-        expect(subject.errors.messages[:email].join(',')).to include("Enter the user's email address")
+        it { is_expected.not_to be_valid }
+
+        it 'has a proper error message' do
+          expect(subject_email_errors).to include("Enter the user's email address")
+        end
       end
-    end
 
-    context 'when email in invalid format with comma' do
-      let(:email) { 'test,a@test.com' }
+      context 'when email with comma' do
+        let(:email) { 'test,a@test.com' }
 
-      it { is_expected.not_to be_valid }
+        it { is_expected.not_to be_valid }
 
-      it 'has a proper error message' do
-        subject.valid?
-        expect(subject.errors.messages[:email].join(',')).to include(
-          'Enter the user’s email address in a valid format'
-        )
+        it 'has a proper error message' do
+          expect(subject_email_errors).to include('Enter the user’s email address in a valid format')
+        end
       end
-    end
 
-    context 'when email in invalid format with two consecutive dots' do
-      let(:email) { 'test..a@test.com ' }
+      context 'when email with two consecutive dots' do
+        let(:email) { 'test..a@test.com ' }
 
-      it { is_expected.not_to be_valid }
+        it { is_expected.not_to be_valid }
 
-      it 'has a proper error message' do
-        subject.valid?
-        expect(subject.errors.messages[:email].join(',')).to include(
-          'Enter the user’s email address in a valid format'
-        )
+        it 'has a proper error message' do
+          expect(subject_email_errors).to include('Enter the user’s email address in a valid format')
+        end
       end
-    end
 
-    context 'when email in invalid format with dot at the end' do
-      let(:email) { 'aaa.@test.com' }
+      context 'when email with dot at the end' do
+        let(:email) { 'aaa.@test.com' }
 
-      it { is_expected.not_to be_valid }
+        it { is_expected.not_to be_valid }
 
-      it 'has a proper error message' do
-        subject.valid?
-        expect(subject.errors.messages[:email].join(',')).to include(
-          'Enter the user’s email address in a valid format'
-        )
+        it 'has a proper error message' do
+          expect(subject_email_errors).to include('Enter the user’s email address in a valid format')
+        end
       end
     end
   end
@@ -92,4 +100,10 @@ describe UsersManagement::AddUserForm, type: :model do
       expect(subject.errors[:email]).to include('Email address already exists')
     end
   end
+end
+
+private
+
+def subject_email_errors
+  subject.errors.messages[:email].join(',')
 end
