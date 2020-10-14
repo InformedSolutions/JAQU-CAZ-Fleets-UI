38f9c5b02e0d6b5e82f60711b397be79706270e9
Piotr Kałamarski <pkalamarski@gmail.com>
2020-10-14 20:51:26 +0200

    [CAZB-2688] Search optimisation - Update matrix search

--- app/javascript/src/payment_matrix/initSearch.js
+++ app/javascript/src/payment_matrix/initSearch.js
@@ -2,22 +2,36 @@ import createHiddenInput from "./createHiddenInput";
 import submitForm from "./submitForm";
 
 export default function () {
-    const searchForm = document.getElementById("search-form");
-    const initClearButton = document.getElementById("clear-search-link");
-    const paymentForm = document.getElementById("payment-form");
+  const searchButton = document.getElementById("search-form-submit");
+  const searchInput = document.getElementById("vrn-search");
+  const clearButton = document.getElementById("clear-search-link");
+  const form = document.getElementById("payment-form");
 
-    searchForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        paymentForm.appendChild(createHiddenInput("commit", "Search"));
-        submitForm(paymentForm);
-    });
+  searchButton.addEventListener("click", (e) => submitForm(e, form));
 
-    if (initClearButton != null) {
-      initClearButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        paymentForm.appendChild(createHiddenInput("commit", "Clear search"));
-        document.getElementById("vrn-search").value = "";
-        submitForm(paymentForm);
-      });
-    }
+  if (!isIE11) {
+    searchInput.addEventListener(
+      "keydown",
+      (e) => e.code === "Enter" && submitForm(e, form)
+    );
+  } else {
+    searchInput.addEventListener(
+      "keydown",
+      (e) => e.keyCode === 13 && submitForm(e, form)
+    );
+  }
+
+  clearButton && initClearButton(clearButton, form);
 }
+
+function initClearButton(clearButton, form) {
+  clearButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    form.appendChild(createHiddenInput("payment[vrn_search]", ""));
+    form.appendChild(createHiddenInput("commit", "Clear search"));
+    document.getElementById("vrn-search").value = "";
+    form.submit();
+  });
+}
+
+const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
