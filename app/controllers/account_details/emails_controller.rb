d03ea248d84879231b0be01b4d08c130ec62e25f
Sergii Chub <s.chub@exlabs.pl>
2020-10-14 10:06:56 +0200

    [CAZB-2639] Redirect to the account details when save no changes (#654)

--- app/controllers/account_details/emails_controller.rb
+++ app/controllers/account_details/emails_controller.rb
@@ -8,34 +8,18 @@ module AccountDetails
   #
   class EmailsController < ApplicationController
     include CheckPermissions
-<<<<<<< HEAD
-    include Devise::Models::RemoteAuthenticatable
-
-    skip_before_action :authenticate_user!, only: %i[confirm_email validate_confirm_email]
-    before_action -> { check_permissions(current_user.owner == true) }, except:
-      %i[confirm_email validate_confirm_email]
-    before_action :set_user_details, only: :edit_email
-
-    ##
-    # Renders the change email email address page for primary users
-=======
 
     before_action -> { check_permissions(current_user.owner == true) }
     before_action :set_user_details, only: :edit
 
     ##
     # Renders the change email email address page for primary users.
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
     #
     # ==== Path
     #
     #    :GET /primary_users/edit_email
     #
-<<<<<<< HEAD
-    def edit_email
-=======
     def edit
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
       @errors = {}
     end
 
@@ -44,88 +28,23 @@ module AccountDetails
     #
     # ==== Path
     #
-    #    :GET /non_primary_users/update_email
+    #    :GET /primary_users/update_email
     #
-<<<<<<< HEAD
     def update_email
-=======
     def update
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
       form = AccountDetails::EditUserEmailForm.new(account_id: current_user.account_id, email: params[:email])
       if form.valid?
         update_owner_email(form.email)
         # TODO: CAZB-2640 - redirect to email sent page
         redirect_to primary_users_account_details_path
+      return redirect_to primary_users_account_details_path if current_user.email == email_params
+
+      form = AccountDetails::EditUserEmailForm.new(account_id: current_user.account_id, email: email_params)
+      if form.valid?
+        update_email_and_redirect(form)
       else
         @errors = form.errors.messages
-<<<<<<< HEAD
-        render :edit_email
-      end
-    end
-
-    ##
-    # Renders the email sent page
-    #
-    # ==== Path
-    #
-    #    :GET /primary_users/email_sent
-    #
-    def email_sent
-      @email = session[:owners_new_email]
-    end
-
-    ##
-    # Call api which will resend a verification email after changing email process
-    #
-    # ==== Path
-    #
-    #    :GET /primary_users/resend_email
-    #
-    def resend_email
-      update_owner_email(session[:owners_new_email])
-      redirect_to email_sent_primary_users_path
-    end
-
-    ##
-    # Renders the email address updated page
-    #
-    # ==== Path
-    #
-    #    :GET /primary_users/confirm_email
-    #
-    # ==== Params
-    # * +token+ - uuid, required in the query
-    #
-    def confirm_email
-      @token = params[:token]
-    end
-
-    ##
-    # Validates the token and password by calling AccountsApi::Users.confirm_email
-    #
-    # ==== Path
-    #
-    #    :GET /primary_users/validate_confirm_email
-    #
-    # ==== Params
-    # * +token+ - token from the email, passes as a hidden param in the form
-    # * +passwords+ - hash
-    #   * +password+ - string, submitted password
-    #   * +password_confirmation+ - string, submitted password confirmation
-    #
-    def validate_confirm_email
-      service = AccountDetails::UpdateEmail.call(
-        password: params.dig(:passwords, :password),
-        password_confirmation: params.dig(:passwords, :password_confirmation),
-        token: params[:token]
-      )
-      if service.valid?
-        sign_in_and_redirect(service)
-      else
-        render_confirm_email(service.errors)
-=======
         render :edit
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
       end
     end
 
@@ -133,25 +52,11 @@ module AccountDetails
 
     # Fetches user details from the API
     def set_user_details
-<<<<<<< HEAD
-      api_response = AccountsApi::Users.user(
-=======
       api_response = AccountsApi.user(
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
         account_id: current_user.account_id,
         account_user_id: current_user.user_id
       )
       @user = AccountDetails::User.new(api_response)
-    end
-
-    # Sends request to API with change email request
-    def update_owner_email(email)
-<<<<<<< HEAD
-      AccountsApi::Auth.update_owner_email(
-        account_user_id: current_user.user_id,
-        new_email: email.downcase,
-        confirm_url: confirm_email_primary_users_url
-      )
     end
 
     # Renders :confirm_email with assigned errors and token
@@ -174,7 +79,6 @@ module AccountDetails
       sign_in(user)
       redirect_to dashboard_path
     end
-=======
       AccountDetails::Api.update_owner_email(
         account_user_id: current_user.user_id,
         new_email: email.downcase,
@@ -182,6 +86,26 @@ module AccountDetails
         confirm_url: primary_users_account_details_path
       )
     end
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
+
+    # Update user email and redirect to the email send page
+    def update_email_and_redirect(form)
+      update_owner_email(form.email)
+      session[:owners_new_email] = form.email
+      redirect_to email_sent_primary_users_path
+    end
+
+    # downcase email params
+    def email_params
+      params[:email].downcase
+    end
+
+    # Sends request to API with change email request
+    def update_owner_email(email)
+      AccountsApi::Auth.update_owner_email(
+        account_user_id: current_user.user_id,
+        new_email: email,
+        confirm_url: confirm_email_primary_users_url
+      )
+    end
   end
 end
--- spec/requests/account_details/emails/update_email_spec.rb
+++ spec/requests/account_details/emails/update_email_spec.rb
@@ -2,42 +2,35 @@
 
 require 'rails_helper'
 
-describe 'AccountDetails::EmailsController - GET #update' do
+describe 'AccountDetails::EmailsController - GET #update_email' do
   subject { get update_email_primary_users_path, params: params }
 
+  before { sign_in user }
+
+  let(:user) { create_owner }
   let(:params) { { email: email } }
   let(:email) { 'carl.gustav@jung.com' }
-
-  before { sign_in create_owner }
 
   context 'when params are valid' do
     context 'when user is successfully updated' do
       before do
-<<<<<<< HEAD
-        allow(AccountsApi::Accounts).to receive(:user_validations).and_return(true)
-        allow(AccountsApi::Auth).to receive(:update_owner_email).and_return(true)
-=======
         allow(AccountsApi).to receive(:user_validations).and_return(true)
         allow(AccountDetails::Api).to receive(:update_owner_email).and_return(true)
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
       end
 
       it 'updates owners email through API' do
         subject
-<<<<<<< HEAD
-        expect(AccountsApi::Auth).to have_received(:update_owner_email)
-=======
         expect(AccountDetails::Api).to have_received(:update_owner_email)
->>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
       end
 
       # TODO: CAZB-2640
       it 'redirects to email sent page'
     end
 
-    context 'when params are not valid' do
-      let(:email) { '' }
+    context 'when user save no changes' do
+      let(:email) { user.email }
 
+<<<<<<< HEAD:spec/requests/account_details/emails/update_spec.rb
 <<<<<<< HEAD
       it 'renders the view' do
         expect(subject).to render_template(:edit_email)
@@ -45,7 +38,19 @@ describe 'AccountDetails::EmailsController - GET #update' do
       it 'renders the edit page' do
         expect(subject).to render_template(:edit)
 >>>>>>> 83c4759... [CAZB-2856] Update email page (#617)
+=======
+      it 'redirects to the account details page' do
+        expect(subject).to redirect_to(primary_users_account_details_path)
+>>>>>>> 4bca06e... [CAZB-2639] Redirect to the account details when save no changes (#654):spec/requests/account_details/emails/update_email_spec.rb
       end
+    end
+  end
+
+  context 'when params are not valid' do
+    let(:email) { '' }
+
+    it 'renders the view' do
+      expect(subject).to render_template(:edit_email)
     end
   end
 end
