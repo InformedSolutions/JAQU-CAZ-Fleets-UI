60794b350d9a1e8b32434075f0ff75e20f6607d8
Piotr Kałamarski <pkalamarski@gmail.com>
2020-10-16 15:43:32 +0200

    [CAZB-2668] Update matrix search - fix pagination (#670)

--- app/javascript/src/payment_matrix/initSearch.js
+++ app/javascript/src/payment_matrix/initSearch.js
@@ -1,23 +1,25 @@
 import createHiddenInput from "./createHiddenInput";
 import submitForm from "./submitForm";
 
+const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+
 export default function () {
   const searchButton = document.getElementById("search-form-submit");
   const searchInput = document.getElementById("vrn-search");
   const clearButton = document.getElementById("clear-search-link");
   const form = document.getElementById("payment-form");
 
-  searchButton.addEventListener("click", (e) => submitForm(e, form));
+  searchButton.addEventListener("click", (e) => submitSearch(e, form));
 
   if (!isIE11) {
     searchInput.addEventListener(
       "keydown",
-      (e) => e.code === "Enter" && submitForm(e, form)
+      (e) => e.code === "Enter" && submitSearch(e, form)
     );
   } else {
     searchInput.addEventListener(
       "keydown",
-      (e) => e.keyCode === 13 && submitForm(e, form)
+      (e) => e.keyCode === 13 && submitSearch(e, form)
     );
   }
 
@@ -25,13 +27,16 @@ export default function () {
 }
 
 function initClearButton(clearButton, form) {
-  clearButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    form.appendChild(createHiddenInput("payment[vrn_search]", ""));
-    form.appendChild(createHiddenInput("commit", "Clear search"));
-    document.getElementById("vrn-search").value = "";
-    form.submit();
-  });
+  clearButton.addEventListener("click", (e) => submitClear(e, form));
 }
 
-const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+function submitSearch(e, form) {
+  form.appendChild(createHiddenInput("commit", "Search"));
+  submitForm(e, form);
+}
+
+function submitClear(e, form) {
+  document.getElementById("vrn-search").value = "";
+  form.appendChild(createHiddenInput("commit", "Clear search"));
+  submitForm(e, form);
+}
