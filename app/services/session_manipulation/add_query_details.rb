4826d4ee6c4dd639e63245e8f487529344d19196
Sergii Chub <s.chub@exlabs.pl>
2021-01-13 19:22:11 +0100

    [CAZB-3756] Fix checkboxes when pagination was used

--- app/services/session_manipulation/add_query_details.rb
+++ app/services/session_manipulation/add_query_details.rb
@@ -13,26 +13,13 @@ module SessionManipulation
     #
     def call
       session[:payment_query] = {}
-      save_search_value if commit == 'SEARCH'
-      save_direction_and_vrn('next') if commit == 'NEXT'
-      save_direction_and_vrn('previous') if commit == 'PREVIOUS'
+      add_search_vrn if params[:commit].upcase == 'SEARCH'
     end
 
     private
 
-    # Extracts commit
-    def commit
-      params[:commit].upcase
-    end
-
-    # Saves direction and vrn
-    def save_direction_and_vrn(direction)
-      session[:payment_query][:direction] = direction
-      session[:payment_query][:vrn] = params.dig(:payment, "#{direction}_vrn")
-    end
-
-    # Saves search value
-    def save_search_value
+    # Saves search value to session
+    def add_search_vrn
       session[:payment_query][:search] = params.dig(:payment, :vrn_search)
     end
   end
