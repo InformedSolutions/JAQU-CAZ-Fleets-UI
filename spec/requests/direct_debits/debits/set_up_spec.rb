8cbc227966562579ab8b905c6b699af729bbd1dd
Antoni <a.pstras@exlabs.pl>
2021-01-13 17:45:19 +0100

    [CAZB-3859] Allow to enter direct debit path with feature flag disabled (#854)

--- spec/requests/direct_debits/debits/set_up_spec.rb
+++ spec/requests/direct_debits/debits/set_up_spec.rb
@@ -35,11 +35,12 @@ describe 'DirectDebits::DebitsController - GET #set_up' do
     before do
       sign_in manage_mandates_user
       mock_direct_debit_disabled
+      mock_debits
       subject
     end
 
     it 'redirects to the not found page' do
-      expect(response).to redirect_to(not_found_path)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
