86013509907598d904916845d95c2d7f36930da4
Sergii Chub <s.chub@exlabs.pl>
2021-02-23 13:40:03 +0100

    [CAZB-4174] Sanitize an account name for page titles (#933)

--- features/support/sign_in_helper.rb
+++ features/support/sign_in_helper.rb
@@ -37,7 +37,7 @@ module SignInHelper
     {
       user_id: options[:user_id] || SecureRandom.uuid,
       account_id: options[:account_id] || SecureRandom.uuid,
-      account_name: options[:account_name] || 'Royal Mail',
+      account_name: options[:account_name] || "Royal Mail's",
       permissions: options[:permissions] || account_permissions,
       days_to_password_expiry: options[:days_to_password_expiry] || 90,
       beta_tester: options[:beta_tester] || false
