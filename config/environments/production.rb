f3c6ffabd94be8847fb0753b77ab22532d7df691
Dawid <d.ratka@exlabs.pl>
2021-04-30 12:51:49 +0200

    [CAZB-4696] Fix Username enumeration through header manipulation (#1061)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,6 +125,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
 
+  # Delete "X-Runtime" header to protect the app from the header timing attack.
+  Rails.application.config.middleware.delete(Rack::Runtime)
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
