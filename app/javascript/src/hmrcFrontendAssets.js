defc855823af33aeb749258cfc37167ad4845f9b
Sergii Chub <s.chub@exlabs.pl>
2021-05-17 15:30:36 +0200

    [CAZB-4099] Load images from `public/media/images` folder

--- app/javascript/src/hmrcFrontendAssets.js
+++ app/javascript/src/hmrcFrontendAssets.js
@@ -1,1 +1,4 @@
+import 'hmrc-frontend/hmrc/components/account-menu/images/icon-chevron-down.svg';
+import 'hmrc-frontend/hmrc/components/account-menu/images/icon-chevron-left.svg';
+import 'hmrc-frontend/hmrc/components/account-menu/images/icon-chevron-right.svg';
 import 'hmrc-frontend/hmrc/components/account-menu/images/icon-home.png';
