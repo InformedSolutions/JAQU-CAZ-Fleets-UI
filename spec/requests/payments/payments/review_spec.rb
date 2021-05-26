64dd3ee46df13de3daa0804225b879e909245cee
Sergii Chub <s.chub@exlabs.pl>
2021-05-26 22:27:33 +0200

    [CAZB-4934] Enable DD payment selection if user is beta tester

--- spec/requests/payments/payments/review_spec.rb
+++ spec/requests/payments/payments/review_spec.rb
@@ -9,51 +9,63 @@ describe 'PaymentsController - GET #review', type: :request do
     before do
       mock_clean_air_zones
       mock_chargeable_vehicles
-      sign_in create_user
       add_to_session(new_payment: { caz_id: mocked_uuid,
                                     details: { mocked_vrn => { dates: %w[2019-11-05 2019-11-06],
                                                                charge: 50 } } })
     end
 
-    it 'calls ComplianceCheckerApi for CAZ name' do
-      subject
-      expect(ComplianceCheckerApi).to have_received(:clean_air_zones)
-    end
+    context 'when user is not beta tester' do
+      context 'when :total_to_pay is < Payments::Constants::CHARGE_LIMIT' do
+        before do
+          sign_in create_user(beta_tester: false)
+          subject
+        end
 
-    it 'assigns :zone variable' do
-      subject
-      expect(assigns(:zone).name).to eq('Birmingham')
-    end
+        it 'calls ComplianceCheckerApi for CAZ name' do
+          expect(ComplianceCheckerApi).to have_received(:clean_air_zones)
+        end
 
-    it 'assigns correct value to session' do
-      subject
-      expect(session.dig(:new_payment, 'direct_debit_enabled')).to be_truthy
-    end
+        it 'assigns :zone variable' do
+          expect(assigns(:zone).name).to eq('Birmingham')
+        end
 
-    it 'assigns :days_to_pay variable' do
-      subject
-      expect(assigns(:days_to_pay)).to eq(2)
-    end
+        it 'assigns correct value to session' do
+          expect(session.dig(:new_payment, 'direct_debit_enabled')).to be_truthy
+        end
 
-    it 'assigns :total_to_pay variable' do
-      subject
-      expect(assigns(:total_to_pay)).to eq(100)
-    end
+        it 'assigns :days_to_pay variable' do
+          expect(assigns(:days_to_pay)).to eq(2)
+        end
 
-    context 'when :total_to_pay is < Payments::Constants::CHARGE_LIMIT' do
-      it 'assigns flash message' do
-        subject
-        expect(flash[:alert]).to eq(nil)
+        it 'assigns :total_to_pay variable' do
+          expect(assigns(:total_to_pay)).to eq(100)
+        end
+
+        it 'not assigns flash message' do
+          expect(flash[:alert]).to eq(nil)
+        end
+      end
+
+      context 'when :total_to_pay is > Payments::Constants::CHARGE_LIMIT' do
+        before do
+          stub_const('Payments::Constants::CHARGE_LIMIT', 0)
+          sign_in create_user(beta_tester: false)
+          subject
+        end
+
+        it 'assigns flash message' do
+          expect(flash[:alert][:limit_exceeded_alert])
+            .to eq(I18n.t('payment_review.errors.limit_exceeded_alert'))
+        end
       end
     end
 
-    context 'when :total_to_pay is > Payments::Constants::CHARGE_LIMIT' do
-      before { stub_const('Payments::Constants::CHARGE_LIMIT', 0) }
+    context 'when user is beta tester' do
+      before { sign_in create_user(beta_tester: true) }
 
-      it 'assings flash message' do
+      it 'assigns correct value to session' do
         subject
-        expect(flash[:alert][:limit_exceeded_alert])
-          .to eq(I18n.t('payment_review.errors.limit_exceeded_alert'))
+        expect(session.dig(:new_payment, 'direct_debit_enabled')).to be_truthy
       end
     end
   end
