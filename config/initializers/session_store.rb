eee30f58908b1c31cafe50e99ddb4b1219543da7
Antoni <a.pstras@exlabs.pl>
2021-06-14 09:54:05 +0200

    [CAZB-4664] Change user timeout mechanism (#1139)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-timeout_in_mins = Rails.configuration.x.session_timeout_in_min.minutes
 store_type = ENV['REDIS_URL'] && !Rails.env.test? ? :cache_store : :cookie_store
 Rails.application.config.session_store store_type,
                                        key: '_caz_fleets_session',
-                                       expire_after: timeout_in_mins,
                                        same_site: :lax,
                                        secure: Rails.env.production?
